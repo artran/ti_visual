fd4f3c8b46a663e50afe02c659ec8e1ae2953ccf
ray <ray@1beba2e3-0727-4f99-a42e-c968e9909459>
2010-05-13 21:26:11 +0000

    Started to create the contact models.
    git-svn-id: https://svn.artran.webfactional.com/ti-visual/trunk@65 1beba2e3-0727-4f99-a42e-c968e9909459

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Element(models.Model):
+    'A component of a contact form.'
+    
+    name = models.CharField(max_length=20, help_text='Memorable name for the component')
+    code = models.CharField(max_length=100, help_text='The django code to produce this component')
+    
+    def __unicode__(self):
+        return self.name
+
+class ContactForm(models.Model):
+    'A collection of form elements that know where to post to.'
+    
+    name = models.CharField(max_length=20, help_text='Memorable name for the form.')
+    recipient_list = models.TextField(help_text='Comma separated list of recipients.')
+    subject_template = models.CharField(max_length=100, help_text='Template code to produce the email subject line.')
+    body_template = models.TextField(help_text='Template code to produce the email body.')
+    elements = models.ManyToManyField(Element, related_name='form', through='ContactFormElements')
+    
+    def __unicode__(self):
+        return self.name
+
+class ContactFormElements(models.Model):
+    'A join model that allows the elements of a form to be ordered.'
+    
+    element = models.ForeignKey(Element)
+    form = models.ForeignKey(ContactForm)
+    sort = models.PositiveSmallIntegerField(help_text='Low numbers sort first.')
+    label = models.CharField(max_length=100, help_text='Label for this element.')
