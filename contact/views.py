3aef1aa6e69f72d18ef13ad02ccf53af7979ced2
ray <ray@1beba2e3-0727-4f99-a42e-c968e9909459>
2010-05-16 19:29:53 +0000

    Playing with exec.
    git-svn-id: https://svn.artran.webfactional.com/ti-visual/trunk@68 1beba2e3-0727-4f99-a42e-c968e9909459

--- contact/views.py
+++ contact/views.py
@@ -5,12 +5,26 @@ from models import *
 
 def contact(request):
     _create_form(1)
+   if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render_to_response('contact.html', {
+        'form': form,
+    })
+ 
 
 def _create_form(form_id):
     contact_form = ContactForm.objects.get(pk=form_id)
     elements = ContactFormElements.objects.filter(form=contact_form).order_by('sort')
 
-    form_str = 'class Auto%sForm(forms.Form):\n' % _clean_string(contact_form.name)
+    form_name = 'Auto%sForm' % _clean_string(contact_form.name)
+    form_str = 'class %s(forms.Form):\n' % form_name
     for element in elements:
         form_str += '    %s = %s\n' % (_clean_string(element.label), element.element.code)
 
