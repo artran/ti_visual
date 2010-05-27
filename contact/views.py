dffd95c52a6a8e237d6d29599b3de81d5df79ef5
ray <ray@1beba2e3-0727-4f99-a42e-c968e9909459>
2010-05-27 21:52:46 +0000

    Made the contact view use the kwarg passed in.
    git-svn-id: https://svn.artran.webfactional.com/ti-visual/trunk@80 1beba2e3-0727-4f99-a42e-c968e9909459

--- contact/views.py
+++ contact/views.py
@@ -6,7 +6,7 @@ from models import *
 
 def contact(request, form_id):
     if request.method == 'POST': # If the form has been submitted...
-        form = ContactForm(1, request.POST) # A form bound to the POST data
+        form = ContactForm(form_id, request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
             return HttpResponseRedirect('/thanks/') # Redirect after POST
