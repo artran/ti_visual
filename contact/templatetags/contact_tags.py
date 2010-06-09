db1f4174b2c863c672ba415ba3f7917a9382ccbd
ray <ray@1beba2e3-0727-4f99-a42e-c968e9909459>
2010-06-09 21:57:36 +0000

    Completed support for the contact_form template tag.
    git-svn-id: https://svn.artran.webfactional.com/ti-visual/trunk@84 1beba2e3-0727-4f99-a42e-c968e9909459

--- contact/templatetags/contact_tags.py
+++ contact/templatetags/contact_tags.py
@@ -1,8 +1,8 @@
 from django.template import Library, Template
 from django.template.context import Context
 
-from forms import ContactForm
-from models import ContactFormModel
+from ti_visual.contact.forms import ContactForm
+from ti_visual.contact.models import ContactFormModel
 
 
 register = Library()
@@ -12,9 +12,9 @@ register = Library()
 def contact_form(form_id):
     try:
         model = ContactFormModel.objects.get(pk=form_id, live=True)
-        form = ContactForm(form_id) # An unbound form
-        template = Template(model.template, self, 'Contact form template for form %s' % form_id)
+        form = ContactForm(form_id)
+        template = Template(model.form_template, name='Contact form template for form %s' % form_id)
         c = Context({'form': form})
         return template.render(c)
     except (ContactFormModel.DoesNotExist):
-        return '<strong>Contact form %s does not exist or is not live.</stron>'
+        return '<strong>Contact form %s does not exist or is not live.</strong>'
